210e47ec6e8150226afb9c17cf23f0421d5c9e26
Nattzor <nathalienlarsson@gmail.com>
2021-02-23 11:18:10 +0100

    inital solution

--- src/javascript/otherStuff.js
+++ src/javascript/otherStuff.js
@@ -1,23 +1,12 @@
-import { createApi } from 'unsplash-js'; 
+import { createApi } from 'unsplash-js';
 
-export async function handleFormSubmit (event) {
-    event.preventDefault();
-    const keyword = this.search.value;
-    // Save search to LocalStorage
-    saveSearchToLocalStorage(keyword);
-    // Fetch Images
-    // const photos = await fetchImages(keyword);
-    // console.log(photos);
-}
-
-function saveSearchToLocalStorage(searchTerm) {
+export function saveSearchToLocalStorage(searchTerm) {
     let savedSearchesArray = JSON.parse(window.localStorage.getItem('savedSearches'));
     savedSearchesArray ? savedSearchesArray.push(searchTerm) : savedSearchesArray = [searchTerm];
     window.localStorage.setItem('savedSearches', JSON.stringify(savedSearchesArray));
 }
 
-
-const fetchImages = async (keyword = 'cats', page = 1) => {
+export const fetchImages = async (keyword = 'cats', page = 1) => {
     const accessKey = 'lyEw-RLUmA-7SQhKVJWXnDZTmzPrB1M9eCD53vZV2IY';
     const api = createApi({
         // Don't forget to set your access token here!
@@ -31,5 +20,11 @@ const fetchImages = async (keyword = 'cats', page = 1) => {
     
     const totalPages = data.response.total_pages;
     const photos = data.response.results;
-    return photos;
+    return photos.map(photo => {
+        return {
+            imageUrl: photo.urls.small,
+            desc: photo.description,
+            altDesc: photo.alt_description
+        }
+    });
 }
